e43456b300f731882a9f2aacb996a8bd509a3482
Abhijithkp2023 <abhijithkp642@gmail.com>
2024-02-22 13:25:51 +0530

    second commit

--- App.js
+++ App.js
@@ -1,18 +1,57 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("div",{id:"parent"},[
-    
-React.createElement("div",{id:"child"},
-   [React.createElement("h2",{},"I am  h2 tag"),
-   React.createElement("h2",{},"I am  h2 tag")
-]),
-React.createElement("div",{id:"child"},
-   [React.createElement("h2",{},"I am  h2 tag"),
-   React.createElement("h2",{},"I am  h2 tag")
-])
- ]);
+/**
+ *  Header
+ *   -Logo
+ *   -Nav-bar
+ * 
+ *  Body
+ *   -Search
+ *   -Res-continer
+ *   -Res-card
+ * 
+ *  Footer
+ *   - Copyright
+ *   - Links
+ *   - Adress 
+ *   - Contacts
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+       <img className="logo-items" src="https://logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header/>
+    </div>
+  )
+}
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-roo.render(heading);
+
+root.render(<AppLayout/>);
       
    
